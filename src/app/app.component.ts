f6ef0d3c3b794d557255ef7e172b5241cc4339e1
Nachd <nachdit.work@gmail.com>
2022-02-01 15:14:35 +0100

    01-02-2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'g4sg1';
+  title = 'TP1 : Binding';
+
+  ch: boolean = true;
+  nb: number = 0;
+
+  objet = {
+    titre: 'tp 1 '
+  }
+
+  //définition d'une fonction
+  test_click() {
+    alert(this.title)
+  }
+
+  inc() {
+    this.nb++
+  }
+  dec() {
+    this.nb--
+  }
+
+  test_keyup(event) {
+    /*  event : keyup
+        target : input
+        value : valeur saisie
+    */
+    console.log(event.target.value)
+  }
+
+  test_change(evt){
+    alert(evt.target.value)
+  }
+
+  
 }
